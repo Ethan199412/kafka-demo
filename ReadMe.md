d6aa92ec099e3af81edbec3d8d247d573c55176c
lijiahao <lijiahao@shopee.com>
2023-11-16 19:19:21 +0800

    readme

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+# mac (brew install)
+## kafka configs
+/usr/local/etc/kafka
+
+## kafka bin
+/usr/local/Cellar/kafka/3.5.1
+
+## kafka logs
+/usr/local/var/lib/kafka-logs
+
+## zookeeper config
+/usr/local/etc/zookeeper
+
+## zookeeper bin
+/usr/local/Cellar/zookeeper/3.9.0
+
+# command
+kafka-server-start /usr/local/etc/kafka/server.properties
+zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties 
--- consumer/src/consumer/consumer.service.ts
+++ consumer/src/consumer/consumer.service.ts
@@ -6,10 +6,16 @@ import {
   Kafka,
 } from 'kafkajs';
 
+// 对于单播消息，如果多个消费者在同一个消费组，那么只有一个消费者可以收到消息。
+// 对于多播，也就是存在多个消费组，那么多个消费者都可以 consume 一个 topic，但是，每个消费组只有一个消费者能收到。
+// mac: /usr/local/var/lib/kafka-logs/
+// 000000.log 保存的就是消息本身
 @Injectable()
 export class ConsumerService {
   private consumer0: Consumer;
   private consumer1: Consumer;
+  private consumer2: Consumer;
+  private consumer3: Consumer;
 
   constructor() {
     const kafka = new Kafka({
@@ -19,6 +25,8 @@ export class ConsumerService {
 
     this.consumer0 = kafka.consumer({ groupId: 'consumer-group' });
     this.consumer1 = kafka.consumer({ groupId: 'consumer-group' });
+    this.consumer2 = kafka.consumer({ groupId: 'consumer-group' });
+    this.consumer3 = kafka.consumer({ groupId: 'consumer-group' });
 
     this.startListening();
   }
@@ -26,11 +34,15 @@ export class ConsumerService {
   async startListening() {
     await this.consumer0.connect();
     await this.consumer1.connect();
+    await this.consumer2.connect();
+    await this.consumer3.connect();
 
     await this.consumer0.subscribe({
       topic: 'topic1',
     });
     await this.consumer1.subscribe({ topic: 'topic1' });
+    await this.consumer2.subscribe({ topic: 'topic1' });
+    await this.consumer3.subscribe({ topic: 'topic1' });
 
     await this.consumer0.run({
       eachMessage: async ({ topic, partition, message }) => {
@@ -44,6 +56,22 @@ export class ConsumerService {
       eachMessage: async ({ topic, partition, message }) => {
         console.log(
           `Consumer 1 - Received message on partition ${partition} of topic ${topic}: ${message.value}`,
+        );
+      },
+    });
+
+    await this.consumer2.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log(
+          `Consumer 2 - Received message on partition ${partition} of topic ${topic}: ${message.value}`,
+        );
+      },
+    });
+
+    await this.consumer3.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log(
+          `Consumer 3 - Received message on partition ${partition} of topic ${topic}: ${message.value}`,
         );
       },
     });
